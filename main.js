6a5176a6e3e1089739356a72d8d37b1206bf51f7
Francisco Dias de Bessa Jr <133795397+Chicaao@users.noreply.github.com>
2024-06-10 21:07:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+
+// Resposta 1
+// // alert('hello world!')
+
+// Resposta 2,6,7 e 8
+  // let numberOne = prompt('insira o primeiro numero')
+  // let numberTwo = prompt('insira o segundo numero')
+
+  // numberOne = Number(numberOne)
+  // numberTwo = Number(numberTwo)
+
+  // let sum = alert(numberOne + numberTwo )
+  // let sub = alert(numberOne - numberTwo)
+  // let multi = alert(numberOne * numberTwo)
+  // let div = alert(numberOne / numberTwo)
+
+// Resposta 3
+  // let anything = '123'
+  // if (typeof anything === 'number') {
+  //   alert('É um numero')
+  // }
+  // else {
+  //   alert('Não é um numero')
+  // }
+
+// Resposta 4
+  // let anything = '123'
+
+  // if (typeof anything === 'string') {
+  //   alert('É uma string')
+  // }
+  // else {
+  //   alert('Não é uma string')
+  // }
+
+// Resposta 5
+  // let anything = false
+
+  // if (typeof anything === 'boolean') {
+  //   alert('É um booleano')
+  // }
+  // else {
+  //   alert('Não é um booleano')
+  // }
+
+// Resposta 9
+  // let number = prompt('insira um numero aqui')
+
+  // number = Number(number)
+
+  // if (number % 2 === 0){
+  //   alert('O numero é par')
+  // // }
+  // else {
+  //   alert('Não é um numero par')
+  // }
+
+  // Resposta 10
+    // let number = prompt('insira um numero aqui')
+
+    // number = Number(number)
+
+    // if (number % 2 !== 0){
+    //   alert('O numero é impar')
+    // }
+    // else {
+    //   alert('Não é um numero impar')
+    // }
